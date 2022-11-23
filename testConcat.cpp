8ea498fea3f5f66efda1bf1e114c5b368823711e
Tuan Kiet <kiet.le0303@hcmut.edu.vn tuankiet0303@gmail.com>
2022-11-24 02:15:05 +0700

    them testcase

--- testConcat.cpp
+++ testConcat.cpp
@@ -2,5 +2,11 @@
 
 int main()
 {
+
+    auto s1 = new ConcatStringTree ("hello");
+    auto s2 =  new ConcatStringTree("world");
+    ConcatStringTree s3 = s1->concat(*s2);
+    delete s1;
+    delete s2;
     return 0;
 }
