c2d81c02add4bc2df9bf54f28e16530a8466d160
Tuan Kiet <kiet.le0303@hcmut.edu.vn tuankiet0303@gmail.com>
2022-11-25 19:17:22 +0700

    Done, còn test kỹ lại

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include "ConcatStringTree.h"
 
-int main() {
+void test(const ConcatStringTree* const p)
+{
+	string s = ( p->toString().substr(18, p->length()) );
+	cout << "String: \"" << s << "\" have the length: "<<p->length() <<endl;
+    cout<<"preorder " << p->toStringPreOrder() << endl;
+	for(auto it = s.begin(); it != s.end(); it++)
+	{
+		cout << "Index of "<<*it<<" = " << p->indexOf(*it) << " get "<< p->get(p->indexOf(*it)) <<endl;
+	}
+}
 
-    ConcatStringTree s1("Hello");
-    ConcatStringTree s2(",_t");
-    ConcatStringTree s3 = s1.concat(s2);
-    cout << s3.toStringPreOrder() << endl;
-    cout << s3.toString() << endl;
-    cout << s3.subString(5, 6).toString() << endl;
-    cout << s3.reverse().toString() << endl;    
+void test(const ReducedConcatStringTree* const p)
+{
+    string s = ( p->toString().substr(18, p->length()) );
+	cout << "String: \"" << s << "\" have the length: "<<p->length() <<endl;
+    cout<<"preorder " << p->toStringPreOrder() << endl;
+	for(auto it = s.begin(); it != s.end(); it++)
+	{
+		cout << "Index of "<<*it<<" = " << p->indexOf(*it) << " get "<< p->get(p->indexOf(*it)) <<endl;
+	}
+}
 
+
+int main(int argc, char *argv[])
+{	
+	ConcatStringTree s1 ("Hello");
+    ConcatStringTree s2 ("world");
+    ConcatStringTree s3 = s1.concat(s2);	
+
+    
     return 0;
 }
