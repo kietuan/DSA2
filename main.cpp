9195fd5d10773442fec71302d57220dd60aa867e
Tuan Kiet <kiet.le0303@hcmut.edu.vn tuankiet0303@gmail.com>
2022-11-28 20:41:57 +0700

    adding testcase

--- main.cpp
+++ main.cpp
@@ -22,13 +22,61 @@ void test(const ReducedConcatStringTree* const p)
 	}
 }
 
+void tcHoc()
+{
+    ConcatStringTree *test1 = new ConcatStringTree("01");
+    ConcatStringTree *test2 = new ConcatStringTree("23");
+    ConcatStringTree *test3 = new ConcatStringTree("45");
+    ConcatStringTree *test4 = new ConcatStringTree("A");
+    ConcatStringTree *test5 = new ConcatStringTree("B");
+    ConcatStringTree *test6 = new ConcatStringTree("C");
+    ConcatStringTree *test7 = new ConcatStringTree("D");
+    ConcatStringTree *test8 = new ConcatStringTree("E");
+    ConcatStringTree *test9 = new ConcatStringTree("F");
+    ConcatStringTree *test10 = new ConcatStringTree("G");
+
+    ConcatStringTree *concat1 = new ConcatStringTree(test1->concat(*test2));
+    ConcatStringTree *concat2 = new ConcatStringTree(test3->concat(*test4));
+    ConcatStringTree *concat3 = new ConcatStringTree(concat1->concat(*concat2));
+    ConcatStringTree *concat4 = new ConcatStringTree(test1->concat(*test5));
+    ConcatStringTree *concat5 = new ConcatStringTree(test1->concat(*test6));
+    ConcatStringTree *concat6 = new ConcatStringTree(test1->concat(*test7));
+    ConcatStringTree *concat7 = new ConcatStringTree(test1->concat(*test8));
+    ConcatStringTree *concat8 = new ConcatStringTree(test1->concat(*test9));
+    ConcatStringTree *concat9 = new ConcatStringTree(test1->concat(*test10));
+    cout << test1->getParTreeStringPreOrder("") << " " << test1->getParTreeSize("") << endl;
+    cout << concat3->getParTreeStringPreOrder("ll") << " " << concat3->getParTreeSize("ll") << endl;
+    cout << concat3->getParTreeStringPreOrder("rr") << " " << concat3->getParTreeSize("rr") << endl;
+    delete test2;
+    delete test3;
+    delete test4;
+    delete test5;
+    delete concat3;
+    delete concat4;
+    delete concat5;
+    delete concat6;
+    delete test6;
+    delete test7;
+    delete test8;
+    delete test9;
+    delete test10;
+
+    delete concat1;
+    cout << test1->getParTreeStringPreOrder("") << " " << test1->getParTreeSize("") << endl;
+    delete concat2;
+    cout << test1->getParTreeStringPreOrder("") << " " << test1->getParTreeSize("") << endl;
+    delete concat7;
+    delete concat8;
+    cout << test1->getParTreeStringPreOrder("") << " " << test1->getParTreeSize("") << endl;
+    delete concat9;
+    cout << test1->getParTreeStringPreOrder("") << " " << test1->getParTreeSize("") << endl;
+    delete test1;
+}
 
 //int main(int argc, char *argv[])
 int main()
 {	
-	ConcatStringTree s1 ("Hello");
-    ConcatStringTree s2 ("world");
-    ConcatStringTree s3 = s1.concat(s2);	
+	tcHoc();
 
     
     return 0;
